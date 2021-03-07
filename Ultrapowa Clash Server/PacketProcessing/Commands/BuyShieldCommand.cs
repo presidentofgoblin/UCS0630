9b093bce6856b978c97c7a66b15f1706096078ed
Magich <75262956+presidentofgoblin@users.noreply.github.com>
2021-03-07 21:40:04 +0700

    Add files via upload

---
+++ Ultrapowa Clash Server/PacketProcessing/Commands/BuyShieldCommand.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System;
+using UCS.Helpers;
+using UCS.Logic;
+
+namespace UCS.PacketProcessing
+{
+    //Commande 0x20A
+    internal class BuyShieldCommand : Command
+    {
+        public BuyShieldCommand(BinaryReader br)
+        {
+            ShieldId = br.ReadUInt32WithEndian(); //= shieldId - 0x01312D00;
+            Unknown1 = br.ReadUInt32WithEndian();
+        }
+        public override void Execute(Level level)
+        {
+            Console.WriteLine(ShieldId);
+            Console.WriteLine(Unknown1);
+        }
+        //00 00 02 0A 01 31 2D 02 00 00 2A EA
+
+        public uint ShieldId { get; set; }
+
+        public uint Unknown1 { get; set; }
+    }
+}
---
+++ Ultrapowa Clash Server/PacketProcessing/Commands/ToggleHeroSleepCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UCS.Helpers;
+using UCS.Logic;
+
+namespace UCS.PacketProcessing
+{
+    //Commande 0x0211
+    internal class ToggleHeroSleepCommand : Command
+    {
+        public ToggleHeroSleepCommand(BinaryReader br)
+        {
+            BuildingId = br.ReadUInt32WithEndian(); //buildingId - 0x1DCD6500;
+            FlagSleep = br.ReadByte();
+            Unknown1 = br.ReadUInt32WithEndian();
+        }
+
+        //00 00 02 11 1D CD 65 06 00 00 01 04 CA
+        public override void Execute(Level level)
+        {
+            System.Console.WriteLine(BuildingId);
+            System.Console.WriteLine(FlagSleep);
+            System.Console.WriteLine(Unknown1);
+        }
+        public uint BuildingId { get; set; }
+
+        public byte FlagSleep { get; set; }
+
+        public uint Unknown1 { get; set; }
+    }
+}
